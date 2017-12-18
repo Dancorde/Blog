8e98b216201bd58fbb2a264805833ccfad285bc5
Danilo Cordeiro <dancorde@gmail.com>
2017-12-18 20:21:40 -0200

    add some messages

--- app/models/author.rb
+++ app/models/author.rb
@@ -20,9 +20,26 @@
 
 class Author < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable,  :registerable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :registerable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+
+  validates_presence_of :name, on: :update
+
+  def change_password(attrs)
+    update(password: attrs[:new_password], password_confirmation: attrs[:new_password_confirmation])
+  end
+
+  def gravatar_image_url
+    "https://www.gravatar.com/avatar/#{gravatar_hash}"
+  end
+
+  private
+
+  def gravatar_hash
+    Digest::MD5.hexdigest(email.downcase)
+  end
+
 end
